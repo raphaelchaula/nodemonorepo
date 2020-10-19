6f20050b4972c911b777aea676868b0ea07ffbde
Raphael Chaula <raphaelchaula@gmail.com>
2020-10-19 03:12:45 +0300

    Add - @nunu/bern

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var { actionOne, actionTwo } = require('@nunu/caashiere');
+var { bernOne, bernTwo, bernThree, bernFour, bernFive } = require('@nunu/bern');
 
 actionOne();
 actionTwo();
+bernOne();
+bernTwo();
+bernThree();
+bernFour();
+bernFive();
